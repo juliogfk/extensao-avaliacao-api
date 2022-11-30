2c624c4eec79e81f37301193964484d90b43515a
juliogfk <juliogfk1@gmail.com>
2022-11-30 17:35:08 -0300

    método get para buscar os dados da tabela e o método get para buscar por um parâmetro. falta put e delete.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Requiring module
 const express  = require('express');
+const mysql = require('mysql2');
+const connect = require('./conexao.js');
 
 //Creating express object
 const app = express();
@@ -7,7 +9,22 @@ const app = express();
 //Handling GET request
 app.get('/', (req, res) => {
     res.send('Esta mensagem foi criada por Julio Gomes Fernandez Kohlhauf')
-    res.end() })
+    res.end() 
+})
+
+app.get('/clientes', (req, res) =>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+        
+    return connect.execSQLQuery('select * from cliente', res);
+})
+
+app.get('/clientes/:id', (req, res) =>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    
+    return connect.execSQLQuery('select * from cliente where id='+ req.params.id, res);
+})
 
 // Port Number
 const PORT = process.env.PORT || 5000;
